be513259fbb87ab2c8a4f1e97f96eba49190ff5f
Nitinsen2710 <90142127+Nitinsen2710@users.noreply.github.com>
2025-07-24 14:49:27 +0530

    Add files via upload

---
+++ task-1.py
@@ -0,0 +1,7 @@
+input1=float(input("Enter the first number:"))
+input2=float(input("Enter the second number:"))
+print()
+print("Addition:",input1+input2)
+print("Substraction:",input1-input2)
+print("Multiplication:",input1*input2)
+print("Division:",input1/input2)
